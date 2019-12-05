59f3a2200dab41015b3f2e623406e4bf8b1c3ebc
Gość <mateuszral7@gmail.com>
2019-12-05 14:55:02 +0100

    asd

--- main.js
+++ main.js
@@ -8,68 +8,50 @@ xhr.addEventListener('load', () => {
         rates.forEach(element => {
             let option = document.createElement('option');
             option.innerText = element.currency.toUpperCase();
-            option.value = element.code;
+            option.value = element.mid;
             document.querySelector('#currency_list').appendChild(option);
         })
     }
     document.querySelector('#currency_list').addEventListener('change', (event) => {
-        let currencyCode = event.target.options[event.target.selectedIndex].value;
-        const xhrCurrency = new XMLHttpRequest();
-        xhrCurrency.open('GET',`http://api.nbp.pl/api/exchangerates/rates/A/${currencyCode}/`,true);
-        xhrCurrency.send();
-        xhrCurrency.addEventListener('load', () => {
-            let json = JSON.parse(xhrCurrency.response);
-            let span = document.createElement('span');
-            let groupDiv = document.createElement('div');
-            let groupDivCourse = document.createElement('div');
-            let moneyLabel = document.createElement('label');
-            let money = document.createElement('input');
-            let course = json.rates[0].mid.toFixed(2);
-            console.log(course);
-            groupDiv.className = 'group';
-            groupDivCourse.className = 'group';
-            moneyLabel.for = 'money';
-            moneyLabel.innerText = 'Ilość pięniędzy:';
-            money.id = 'money';
-            money.type = 'number';
-            span.innerText = `Kurs: ${course} zł`;
-            span.id = 'course';
+        let course = event.target.selectedOptions[0].value;
+        console.log(Number(course));
+        console.log(typeof course);
+        // console.log(Math.round(course*100)/100);
+        let span = document.createElement('span');
+        let groupDiv = document.createElement('div');
+        let groupDivCourse = document.createElement('div');
+        let moneyLabel = document.createElement('label');
+        let money = document.createElement('input');
+        groupDiv.className = 'group';
+        groupDivCourse.className = 'group';
+        moneyLabel.for = 'money';
+        moneyLabel.innerText = 'Ilość pięniędzy:';
+        money.id = 'money';
+        money.type = 'number';
+        span.innerText = `Kurs: ${course} zł`;
+        span.id = 'course';
 
-            // count = (event) => {
-            //     if(document.querySelector('#owned_money') == null) {
-            //         let result = document.createElement('span');
-            //         result.id = 'owned_money';
-            //         result.innerText = `Masz: ${(event.target.value * course).toFixed(2)} zł`;
-            //         resultDiv.appendChild(result);
-            //     } else {
-            //         document.querySelector('#owned_money').innerText = `Masz: ${(event.target.value * course).toFixed(2)} zł`;
-            //     }
-            // }
-            // money.addEventListener('keyup', count);
-
-            count = (event) => {
-                if(document.querySelector('#owned_money') == null) {
-                    let result = document.createElement('span');
-                    result.id = 'owned_money';
-                    result.innerText = `Masz: ${(event.target.value * course).toFixed(2)} zł`;
-                    resultDiv.appendChild(result);
-                } else {
-                    document.querySelector('#owned_money').innerText = `Masz: ${(event.target.value * course).toFixed(2)} zł`;
-                }
-            }
-            document.querySelector('.main').addEventListener('keyup', count);
-
-            if(resultDiv.childElementCount == 0) {
-                groupDiv.appendChild(moneyLabel);
-                groupDiv.appendChild(money);
-    
-                resultDiv.appendChild(groupDiv);
-                groupDivCourse.appendChild(span);
-                resultDiv.appendChild(groupDivCourse);
+        count = (event) => {
+            if(document.querySelector('#owned_money') == null) {
+                let result = document.createElement('span');
+                result.id = 'owned_money';
+                result.innerText = `Masz: ${(event.target.value * course).toFixed(2)} zł`;
+                resultDiv.appendChild(result);
             } else {
-                document.querySelector('#course').innerText = `Kurs: ${course} zł`;
+                document.querySelector('#owned_money').innerText = `Masz: ${(event.target.value * course).toFixed(2)} zł`;
             }
-        })
+        }
+        document.querySelector('.main').addEventListener('keyup', count);
+
+        if(resultDiv.childElementCount == 0) {
+            groupDiv.appendChild(moneyLabel);
+            groupDiv.appendChild(money);
+            resultDiv.appendChild(groupDiv);
+            groupDivCourse.appendChild(span);
+            resultDiv.appendChild(groupDivCourse);
+        } else {
+            document.querySelector('#course').innerText = `Kurs: ${course} zł`;
+        }
     })
 })
 
